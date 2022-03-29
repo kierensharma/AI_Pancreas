e60d8d53207a0ce5238b18d40d251e97f2f17494
kierensharma <kieren.sharma@gmail.com>
2022-03-29 13:20:36 +0100

    Working data extractor

--- data_extractor.py
+++ data_extractor.py
@@ -1,3 +1,4 @@
+from math import comb
 import os
 from matplotlib.pyplot import axis
 import pandas as pd
@@ -17,14 +18,15 @@ for file in sorted(data_files):
 
 combined_dfs = pd.concat(dfs, axis=0, ignore_index=True)
 combined_dfs.columns = ['Date', 'Time', 'Code', 'Value']
-print(combined_dfs)
 
 combined_dfs["Date-time"] = combined_dfs["Date"] + ' ' + combined_dfs["Time"]
 combined_dfs.drop(columns=['Date', 'Time'], axis=1, inplace=True)
-print(combined_dfs)
 
 # convert to date
 combined_dfs['Date-time'] = pd.to_datetime(combined_dfs['Date-time'])
+combined_dfs = combined_dfs.dropna()
 combined_dfs.set_index('Date-time', inplace=True)
-combined_dfs.sort_values(by='Date-time', ascending=True)
+combined_dfs.sort_values(by='Date-time', ascending=True, inplace=True)
 print(combined_dfs)
+
+combined_dfs.to_csv('concat_data.csv')
