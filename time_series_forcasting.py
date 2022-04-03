50bedce180b4f6af9e461fb10ce3e0e25efed6ef
kierensharma <kieren.sharma@gmail.com>
2022-04-03 19:30:26 +0100

    Changed names

---
+++ time_series_forcasting.py
@@ -0,0 +1,147 @@
+import os
+import time
+import pandas as pd
+import datetime
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tqdm import tqdm
+
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
+def split_window(self, features):
+  inputs = features[:, self.input_slice, :]
+  labels = features[:, self.labels_slice, :]
+  if self.label_columns is not None:
+    labels = tf.stack(
+        [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+        axis=-1)
+
+  # Slicing doesn't preserve static shape information, so set the shapes
+  # manually. This way the `tf.data.Datasets` are easier to inspect.
+  inputs.set_shape([None, self.input_width, None])
+  labels.set_shape([None, self.label_width, None])
+
+  return inputs, labels
+
+def plot(self, model=None, plot_col='Blood sugar', max_subplots=3):
+  inputs, labels = self.example
+  plt.figure(figsize=(12, 8))
+  plot_col_index = self.column_indices[plot_col]
+  max_n = min(max_subplots, len(inputs))
+  for n in range(max_n):
+    plt.subplot(max_n, 1, n+1)
+    plt.ylabel(f'{plot_col} [normed]')
+    plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+             label='Inputs', marker='.', zorder=-10)
+
+    if self.label_columns:
+      label_col_index = self.label_columns_indices.get(plot_col, None)
+    else:
+      label_col_index = plot_col_index
+
+    if label_col_index is None:
+      continue
+
+    plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                edgecolors='k', label='Labels', c='#2ca02c', s=64)
+    if model is not None:
+      predictions = model(inputs)
+      plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                  marker='X', edgecolors='k', label='Predictions',
+                  c='#ff7f0e', s=64)
+
+    if n == 0:
+      plt.legend()
+
+  plt.xlabel('Time [h]')
+
+
+
+def main():
+    df = pd.read_csv('interpolated.csv', parse_dates=[0])
+    df.pop('Code')
+
+    # Adds column with rough carb intake
+    df.loc[df['Event'] == 'Breakfast', 'Carbs'] = 40
+    df.loc[df['Event'] == 'Lunch', 'Carbs'] = 50
+    df.loc[df['Event'] == 'Dinner', 'Carbs'] = 75
+    df.loc[df['Event'] == 'Snack', 'Carbs'] = 10
+    df.pop('Event')
+    df['Carbs'] = df['Carbs'].fillna(0)
+
+    date_time = pd.to_datetime(df.pop('Date-time'))
+    timestamp_s = date_time.map(pd.Timestamp.timestamp)
+
+    # Creation of 'time of day signal' due to periodicity of data
+    day = 24*60*60
+
+    df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+    df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+
+    # Splitting of full dataframe into training, validation and testing sets
+    column_indices = {name: i for i, name in enumerate(df.columns)}
+
+    n = len(df)
+    train_df = df[0:int(n*0.7)]
+    val_df = df[int(n*0.7):int(n*0.9)]
+    test_df = df[int(n*0.9):]
+
+    num_features = df.shape[1]
+
+    # Normilisation of data to scale features
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std
+    test_df = (test_df - train_mean) / train_std
+
+    # Data Windowing
+    w1 = WindowGenerator(input_width=24, label_width=1, shift=24,
+                     label_columns=['Blood-sugar'])
+
+    w2 = WindowGenerator(input_width=6, label_width=1, shift=1,
+                     label_columns=['Blood-sugar'])
+
+
+
+if __name__ == '__main__':
+    main()
